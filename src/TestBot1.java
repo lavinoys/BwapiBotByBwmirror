8a4470852fe7b7243df56541ca43bed25889001b
mjk <grmjkzz@gmail.com>
2017-05-16 16:49:46 +0900

    서플 추가 로직

--- src/TestBot1.java
+++ src/TestBot1.java
@@ -15,6 +15,57 @@ public class TestBot1 extends DefaultBWListener {
         mirror.startGame();
     }
 
+    // Returns a suitable TilePosition to build a given building type near
+    // specified TilePosition aroundTile, or null if not found. (builder parameter is our worker)
+    public TilePosition getBuildTile(Unit builder, UnitType buildingType, TilePosition aroundTile) {
+        TilePosition ret = null;
+        int maxDist = 3;
+        int stopDist = 40;
+
+        // Refinery, Assimilator, Extractor
+        if (buildingType.isRefinery()) {
+            for (Unit n : game.neutral().getUnits()) {
+                if ((n.getType() == UnitType.Resource_Vespene_Geyser) &&
+                        ( Math.abs(n.getTilePosition().getX() - aroundTile.getX()) < stopDist ) &&
+                        ( Math.abs(n.getTilePosition().getY() - aroundTile.getY()) < stopDist )
+                        ) return n.getTilePosition();
+            }
+        }
+
+        while ((maxDist < stopDist) && (ret == null)) {
+            for (int i=aroundTile.getX()-maxDist; i<=aroundTile.getX()+maxDist; i++) {
+                for (int j=aroundTile.getY()-maxDist; j<=aroundTile.getY()+maxDist; j++) {
+                    if (game.canBuildHere(new TilePosition(i,j), buildingType, builder, false)) {
+                        // units that are blocking the tile
+                        boolean unitsInWay = false;
+                        for (Unit u : game.getAllUnits()) {
+                            if (u.getID() == builder.getID()) continue;
+                            if ((Math.abs(u.getTilePosition().getX()-i) < 4) && (Math.abs(u.getTilePosition().getY()-j) < 4)) unitsInWay = true;
+                        }
+                        if (!unitsInWay) {
+                            return new TilePosition(i, j);
+                        }
+                        // creep for Zerg
+                        if (buildingType.requiresCreep()) {
+                            boolean creepMissing = false;
+                            for (int k=i; k<=i+buildingType.tileWidth(); k++) {
+                                for (int l=j; l<=j+buildingType.tileHeight(); l++) {
+                                    if (!game.hasCreep(k, l)) creepMissing = true;
+                                    break;
+                                }
+                            }
+                            if (creepMissing) continue;
+                        }
+                    }
+                }
+            }
+            maxDist += 2;
+        }
+
+        if (ret == null) game.printf("Unable to find suitable build position for "+buildingType.toString());
+        return ret;
+    }
+
     @Override
     public void onUnitCreate(Unit unit) {
         System.out.println("New unit discovered " + unit.getType());
@@ -24,6 +75,8 @@ public class TestBot1 extends DefaultBWListener {
     public void onStart() {
         game = mirror.getGame();
         self = game.self();
+
+        game.setLocalSpeed(35);//게임 속도 30이 기본, 토너먼트에선 20
 
         //Use BWTA to analyze map
         //This may take a few minutes if the map is processed first time!
@@ -75,6 +128,30 @@ public class TestBot1 extends DefaultBWListener {
                 //if a mineral patch was found, send the worker to gather it
                 if (closestMineral != null) {
                     myUnit.gather(closestMineral, false);
+                }
+            }
+
+            //print TilePosition and Position of my SCVs
+            if (myUnit.getType() == UnitType.Terran_SCV) {
+                game.drawTextMap(myUnit.getPosition().getX(), myUnit.getPosition().getY(), myUnit.getOrder().toString());
+                game.drawLineMap(myUnit.getPosition().getX(), myUnit.getPosition().getY(), myUnit.getOrderTargetPosition().getX(),
+                        myUnit.getOrderTargetPosition().getY(), bwapi.Color.Black);
+            }
+        }
+
+        //if we're running out of supply and have enough minerals ...
+        if ((self.supplyTotal() - self.supplyUsed() < 2) && (self.minerals() >= 100)) {
+            //iterate over units to find a worker
+            for (Unit myUnit : self.getUnits()) {
+                if (myUnit.getType() == UnitType.Terran_SCV) {
+                    //get a nice place to build a supply depot
+                    TilePosition buildTile =
+                            getBuildTile(myUnit, UnitType.Terran_Supply_Depot, self.getStartLocation());
+                    //and, if found, send the worker to build it (and leave others alone - break;)
+                    if (buildTile != null) {
+                        myUnit.build(UnitType.Terran_Supply_Depot, buildTile);
+                        break;
+                    }
                 }
             }
         }
